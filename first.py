6e86d7812de1dfc6bdc1d51cef70c5d8f122faa8
ogola-eddy <122253158+ogola-eddy@users.noreply.github.com>
2023-01-18 01:27:02 -0500

    Arithmetic operators

---
+++ first.py
@@ -0,0 +1,25 @@
+# addition
+print("2 + 3:", 2 + 3)
+
+# subtraction
+print("6 - 2:", 6 - 2)
+
+# multiplication
+print("23 * 56:", 23 * 56)
+
+# division
+print("4.834 / 2:", 4.834 / 2)
+
+# floor division
+print("3.5 // 2:", 3.5 // 2)
+print("4 // 2:", 4 // 2)
+
+# square root
+print("square root of 49:", 49**0.5)
+
+# remainder
+print("Remainder of 5/2:", 5 % 2)
+
+x = 1
+ans = x**2 + x**3 + x**4
+print("polynomial:", ans)
